e621f62d1aab7f970f4a7bd1524519b34a3526f7
prince <A00229731@mycambrian.ca>
2021-10-13 23:43:24 -0400

    Lab4 done

---
+++ Lab4/Surface.cs
@@ -0,0 +1,35 @@
+﻿using Lab4.Geometry2D;
+using Lab4.Particles;
+
+namespace Lab4.ModelComponents
+{
+	public interface ISurface
+	{
+		Cell HandlePhonon(Phonon p);
+	}
+
+	public class BoundarySurface : ISurface
+	{
+		public SurfaceLocation Location { get; }
+		protected Cell cell;
+
+		public BoundarySurface(SurfaceLocation location, Cell cell)
+		{
+			Location = location;
+			this.cell = cell;
+		}
+		public virtual Cell HandlePhonon(Phonon p)
+		{
+			Vector direction = p.Direction;
+			if (Location == SurfaceLocation.left || Location == SurfaceLocation.right)
+			{
+				p.SetDirection(-direction.DX, direction.DY);
+			}
+			else
+			{
+				p.SetDirection(direction.DX, -direction.DY);
+			}
+			return cell;
+		}
+	}
+}
